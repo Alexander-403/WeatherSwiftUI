3d4c0a88752eac266b3543194f7e447770bce1a8
d <d>
2026-02-25 14:30:19 +0300

    Initial Commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Alexander B on 25.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
